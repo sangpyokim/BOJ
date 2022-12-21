e993a6a2024a6a269f405a1b6a5abf38017c3dbf
sangpyokim <59750729+sangpyokim@users.noreply.github.com>
2022-12-21 21:46:38 +0900

    [Silver I] Title: 신입 사원, Time: 4404 ms, Memory: 461372 KB -BaekjoonHub

---
+++ 백준/Silver/1946. 신입 사원/신입 사원.js
@@ -0,0 +1,18 @@
+let fs = require('fs');
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const [T,...input] = fs.readFileSync(filePath).toString().trim().split('\n');
+
+for(let i=0;i<T;i++) {
+  let N = input.shift();
+  let Case = input.splice(0,N).map(v=>v.split(' ').map(Number)).sort((a,b) => a[0]-b[0]);
+  let answer = 1;
+  let top = Case[0][1];
+  for(let i=1;i<N;i++) {
+    if(Case[i][1]<top) {
+      top = Case[i][1];
+      answer++;
+    }
+    if(top===1) break;
+  }
+  console.log(answer);
+}
