2612a50ccc1eb01809e5bd08a4ad891da6f93887
sangpyokim <59750729+sangpyokim@users.noreply.github.com>
2022-12-26 14:39:42 +0900

    [Gold IV] Title: 네트워크 연결, Time: 456 ms, Memory: 46336 KB -BaekjoonHub

---
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+const row = +input.shift()
+const col = +input.shift()
+const list = input.map((i) => i.split(' ').map(Number));
+
+list.sort((a, b) => a[2] - b[2])
+
+const par = Array.from({ length: row + 1 }, (_, i) => i)
+const find = x => x === par[x] ? x : par[x] = find(par[x])
+const union = (x, y) => {
+  if (x > y) return union(y, x)
+  x = find(x)
+  y = find(y)
+
+  par[y] = x
+}
+
+
+let cost = 0 
+for (let [x, y, w] of list) {
+  if (find(x) !== find(y)) {
+    union(x, y)
+    cost += w
+  }
+}
+console.log(cost)
