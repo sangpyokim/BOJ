4944ddf0e5b3b1a915a591b5de6afb4003dad3ff
sangpyokim <59750729+sangpyokim@users.noreply.github.com>
2022-12-27 17:58:39 +0900

    [Gold IV] Title: 부분합, Time: 2992 ms, Memory: 20844 KB -BaekjoonHub

---
+++ 백준/Gold/1806. 부분합/부분합.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+const [N, target] = input.shift().split(' ').map(Number)
+const list = input[0].split(' ').map(Number)
+
+const table = []
+table[0] = list[0]
+for (let i = 1; i < N; i++) {
+  table[i] = list[i] + table[i - 1]
+}
+
+let answer = Infinity
+for (let startIndex = 0; startIndex < N; startIndex++) {
+  const cur = table[startIndex]
+
+  for (let j = startIndex; j >= 0; j--) {
+    const prev = table[j]
+    const prefix = cur - prev
+    if (prefix >= target) {
+      if (answer > startIndex - j) {
+        answer = startIndex - j
+      } else {
+        break;
+      }
+    }
+  }
+  if (cur >= target) {
+    answer = Math.min(answer, startIndex+1)
+  }
+}
+console.log(answer > N ? 0 : answer)
