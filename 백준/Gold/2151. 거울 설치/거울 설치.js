74de56a30c8a802514ef7930cc7651686d172140
sangpyokim <59750729+sangpyokim@users.noreply.github.com>
2023-01-19 19:51:31 +0900

    [Gold III] Title: 거울 설치, Time: 224 ms, Memory: 15084 KB -BaekjoonHub

--- 백준/Gold/2151. 거울 설치/거울 설치.js
+++ 백준/Gold/2151. 거울 설치/거울 설치.js
@@ -1,83 +1,9 @@
-class Node {
-  constructor(weight, val) {
-    this.weight = weight
-    this.val = val
-  }
-}
-class PriorityQueue {
-  constructor() {
-    this.arr = []
-  }
-
-  enqueue(weight, val) {
-    const newNode = new Node(weight, val)
-    this.arr.push(newNode)
-
-    let index = this.arr.length-1
-    // 부모보다 작으면 바꾸기
-    while (true) {
-      const parIndex = index >>> 1
-      const par = this.arr[parIndex]
-      if (par.weight <= newNode.weight) break;
-
-      [this.arr[parIndex], this.arr[index]] = [newNode, par]
-
-      index = parIndex
-    }
-  }
-
-  dequeue() {
-    const res = this.arr[0]
-    const tail = this.arr.pop()
-    const len = this.arr.length
-
-    if (len === 0) return res
-
-    this.arr[0] = tail
-    let index = 0
-    const cur = this.arr[index]
-
-    while (true) {
-      const leftChildIndex = index * 2 + 1
-      const rightChildIndex = index * 2 + 2
-
-      if (rightChildIndex >= len) break;
-
-      let leftChild = this.arr[leftChildIndex]
-      let rightChild = this.arr[rightChildIndex]
-      let swap = null
-
-
-      if (leftChild.weight < cur.weight) {
-        swap = leftChildIndex
-      }
-
-      if (rightChild.weight < cur.weight && rightChild.weight < leftChild.weight) {
-        swap = rightChildIndex
-      }
-
-      if (!swap) break;
-
-      [ this.arr[index], this.arr[swap] ] = [this.arr[swap], cur]
-      index = swap
-    }
-
-    return res
-  }
-
-}
-
 const fs = require('fs');
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 const input = fs.readFileSync(filePath).toString().trim().split("\n");
 
 const N = +input.shift()
 const list = input.map(l => l.split(''))
-
-
-// 거울을 몇번 사용햇냐
-// ------------------
-let answer = 0
 
 const dir = [[-1,0], [1,0], [0,-1], [0,1]] // 상 하 좌 우
 const isVal = (x, y) => x >= 0 && x < N && y >= 0 && y < N
@@ -95,7 +21,7 @@ for (let i = 0; i < 4; i++) {
   dist[sx][sy][i] = 0
   q.push([sx, sy, i])
 }
-// list[sx][sy] = '!'
+
 while (q.length) {
   const temp = []
 
@@ -103,11 +29,9 @@ while (q.length) {
     const [x, y] = dir[d]
     const dx = i + x
     const dy = j + y
-    // console.log(dx, dy)
 
     if (!isVal(dx, dy) || list[dx][dy] === '*') continue
 
-    // console.log(dx, dy)
 
     if (list[dx][dy] === '.') {
       if (dist[dx][dy][d] > dist[i][j][d]) dist[dx][dy][d] = dist[i][j][d]
