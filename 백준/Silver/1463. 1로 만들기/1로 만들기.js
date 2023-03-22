c92fc1161877d1860f168fedab4e2774b125198e
sangpyokim <59750729+sangpyokim@users.noreply.github.com>
2023-03-22 09:53:25 +0900

    [Silver III] Title: 1로 만들기, Time: 236 ms, Memory: 41036 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.js
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.js
@@ -1,41 +1,16 @@
-// /dev/stnin
-// 한 줄 입력
-let fs = require('fs');
-let input = fs.readFileSync('/dev/stdin').toString().split(' ');
+const fs = require('fs');
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-let num = Number(input);
-
+const N = +input.shift()
 
 const dp = []
 dp[0] = 0
 dp[1] = 0
-dp[2] = 1
-dp[3] = 1
 
-for (let i = 4; i <= num; i++) {
-  if (isThree(i) && isTwo(i)) {
-    const three = i / 3
-    const two = i / 2
-    dp[i] = 1 + Math.min(dp[i-1], dp[two], dp[three])
-  }
-  else if (isThree(i)) {
-    const three = i / 3
-    dp[i] = 1 + Math.min(dp[i-1], dp[three])
-  }
-  else if (isTwo(i)) {
-    const two = i / 2
-    dp[i] = 1 + Math.min(dp[i-1], dp[two])
-  } else {
-    dp[i] = 1 + dp[i-1]
-  }
+for (let i = 2; i <= N; i++) {
+  dp[i] = dp[i-1] + 1
+  if (i % 2 === 0) dp[i] = Math.min(dp[i/2]+1,dp[i])
+  if (i % 3 === 0) dp[i] = Math.min(dp[i/3]+1,dp[i])
 }
-
-console.log(dp[num])
-
-function isThree(num) {
-  return num % 3 === 0
-}
-
-function isTwo(num) {
-  return num % 2 === 0
-}
+console.log(dp[N])
