011edefcda7aecb2b7927992b1e4c73ae894a763
sangpyokim <59750729+sangpyokim@users.noreply.github.com>
2023-03-24 16:23:39 +0900

    [Silver I] Title: 카드 구매하기, Time: 188 ms, Memory: 9692 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+const N = +input.shift()
+const list = input.shift().split(' ').map(Number)
+list.unshift(0)
+const dp = []
+dp[0] = list[0]
+dp[1] = list[1]
+
+for (let i = 2; i <= N; i++) {
+  let max = list[i], diff = 0
+
+  for (let j = i; j >= 1; j--) {
+    max = Math.max(max, list[j] + dp[diff])
+
+    diff += 1
+  }
+
+  dp[i] = max
+}
+console.log(dp[N])
