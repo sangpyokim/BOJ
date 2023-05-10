92ebe074db134e68b46fc477b7ccac894d98b91d
sangpyokim <59750729+sangpyokim@users.noreply.github.com>
2023-05-10 14:29:12 +0900

    [Silver I] Title: 스타트링크, Time: 1172 ms, Memory: 52924 KB -BaekjoonHub

---
+++ 백준/Silver/5014. 스타트링크/스타트링크.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+const [F, S, G, U, D] = input.shift().split(' ').map(Number)
+
+const solution = () => {
+  let q = [S], time = 0
+  const dir = [U, -D]
+  const isVal = x => x >= 1 && x <= F
+
+  const set = new Set()
+  set.add(S)
+
+  while (q.length) {
+    const temp = []
+    
+    for (let cur of q) {
+      if (cur === G) return console.log(time)
+
+      for (let x of dir) {
+        const dx = cur + x
+
+        if (isVal(dx) && !set.has(dx)) {
+          set.add(dx)
+          temp.push(dx)
+        }
+      }
+    }
+
+    q = temp
+    time += 1
+  }
+
+  return console.log('use the stairs')
+}
+
+solution()
